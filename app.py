127b742c557a486b5f4a0698d5173d7b44022299
mattlimsiaco <matt.limsiaco@stryker.com>
2025-07-25 15:28:24 -0500

    frontend test

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, jsonify
-from model import predict_fda_code 
+from flask import Flask, request, jsonify, send_from_directory
+from model import predict_fda_code
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def serve_frontend():
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -14,6 +19,7 @@ def predict():
 
     result = predict_fda_code(event_desc, prod_id)
     return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
